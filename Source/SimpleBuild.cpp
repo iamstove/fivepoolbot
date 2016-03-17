59ffc8dd2f81cc7e37bc590a7d77c911470c0a70
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2016-03-17 00:10:29 -0400

    actually builds zerglings and shit

--- Source/SimpleBuild.cpp
+++ Source/SimpleBuild.cpp
@@ -13,10 +13,10 @@ bool buildPool() {
 	return false;
 }
 int unitBuild() {  // 0 is worker or overlord,  1 is zergling
-	if (BWAPI::Broodwar->self()->supplyUsed() >= 10 && !(BWAPI::Broodwar->self()->supplyUsed() >= BWAPI::Broodwar->self()->supplyTotal())) {
+	if (BWAPI::Broodwar->self()->supplyUsed() >= 10 && !(BWAPI::Broodwar->self()->supplyUsed() >= BWAPI::Broodwar->self()->supplyTotal())) {//supply is greater than 4.5 and is not greater than available
 		return 1;
 	}
-	else if (!Broodwar->self()->hasUnitTypeRequirement(BWAPI::UnitTypes::Zerg_Zergling) && Broodwar->self()->minerals() <= 200) {
+	else if (BWAPI::Broodwar->self()->supplyUsed() >= 12 && !Broodwar->self()->hasUnitTypeRequirement(BWAPI::UnitTypes::Zerg_Zergling) && Broodwar->self()->minerals() <= 210) {//this makes it save on minerals til spawning pool
 		return 1;
 	}
 	else {
